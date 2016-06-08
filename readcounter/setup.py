f65bbd4201d8db78efaa86187789c08073247de9
Ilia Kats <ilia-kats@gmx.net>
2016-06-08 17:47:41 +0200

    switch to setuptools

--- readcounter/setup.py
+++ readcounter/setup.py
@@ -1,5 +1,5 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup
+from setuptools.extension import Extension
 from Cython.Build import cythonize
 
 setup(ext_modules = cythonize(Extension("readcounter",
