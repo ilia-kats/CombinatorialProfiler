1ca16746472501ca1ad80d89d5658341d8c8205e
Ilia Kats <ilia-kats@gmx.net>
2016-06-10 16:38:09 +0200

    add test for named inserts

--- tests/test_readcounting.py
+++ tests/test_readcounting.py
@@ -18,7 +18,7 @@ class FastQCreator:
         return seq.translate(cls.ttable)[::-1]
 
     def __init__(self, tmpdir, ninserts=2):
-        self.totalreads = random.randrange(10000, 100001)
+        self.totalreads = random.randrange(1000, 10001)
         self.fwcodes = set()
         self.revcodes = set()
         self.inserts = set()
@@ -26,6 +26,7 @@ class FastQCreator:
         self.varlength = random.randrange(6, 10)
 
         self.varcounts = [{} for i in range(ninserts)]
+        self.unique_inserts = set()
 
         self.file = tmpdir.join("%i.fastq" % time.time())
 
@@ -51,6 +52,7 @@ class FastQCreator:
                 ins = random.randrange(0, len(self.inserts))
                 varins = "".join([self.getnuc() for n in range(self.varlength)])
                 insert = self.inserts[ins].replace("N" * self.varlength, varins)
+                self.unique_inserts.add(varins)
 
                 fwi = random.randrange(0, len(self.fwcodes))
                 revi = random.randrange(0, len(self.revcodes))
@@ -63,22 +65,52 @@ class FastQCreator:
                 print("+", file=fq)
                 print("I" * (len(fwcode) + len(insert) + len(revcode)), file=fq)
 
-def test_init(tmpdir):
-    global fq
+def test_simplecounts(tmpdir):
     fq = FastQCreator(tmpdir)
-
-def test_counts(tmpdir):
     exps = []
     for i in enumerate(fq.inserts):
         d = {}
         d['insert'] = i[1]
-        d['barcodes_fw'] = dict((str(i), c) for i,c in enumerate(fq.fwcodes))
-        d['barcodes_rev'] = dict((str(i), c) for i,c in enumerate(fq.revcodes))
+        d['barcodes_fw'] = {str(i): c for i,c in enumerate(fq.fwcodes)}
+        d['barcodes_rev'] = {str(i): c for i,c in enumerate(fq.revcodes)}
         exps.append(PyExperiment(str(i[0]), d))
     counter = PyReadCounter(exps)
 
     unmatched = tmpdir.mkdir("%s_unmatched" % fq.file.basename)
     counter.countReads(str(fq.file), str(unmatched), 4)
 
+    assert counter.read == fq.totalreads
+    assert counter.counted == fq.totalreads
     for val, truth in zip(exps, fq.varcounts):
         assert val.counts == truth
+
+def test_namedinserts(tmpdir):
+    fq = FastQCreator(tmpdir, ninserts=1)
+    ncounts = {}
+    totalcounts = 0
+    unmatched = 0
+    nins = {i: i for i in random.sample(fq.unique_inserts, int(0.8 * len(fq.unique_inserts)))}
+    for codes, vals in fq.varcounts[0].items():
+        ncounts[codes] = {}
+        for ins, counts in vals.items():
+            if ins in nins:
+                ncounts[codes][ins] = counts
+                totalcounts += counts
+            else:
+                unmatched += counts
+    d = {'insert': fq.inserts[0], 'barcodes_fw': dict((str(i), c) for i,c in enumerate(fq.fwcodes)), 'barcodes_rev': dict((str(i), c) for i,c in enumerate(fq.revcodes)), 'named_inserts': nins}
+    exp = PyExperiment('test', d)
+    counter = PyReadCounter([exp])
+
+    unmatcheddir = tmpdir.mkdir("%s_unmatched" % fq.file.basename)
+    counter.countReads(str(fq.file), str(unmatcheddir), 4)
+
+    assert counter.read == fq.totalreads
+    for codes, vals in exp.counts.items():
+        for i, c in vals.items():
+            assert i in nins
+
+    assert counter.counted == totalcounts
+    assert counter.unmatched_insert_sequence == unmatched
+    assert counter.unmatched_total == counter.unmatched_insert_sequence
+    assert exp.counts == ncounts
