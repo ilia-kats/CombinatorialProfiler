347ea7ec119d5678efc19c776e0cefe7d2515114
Ilia Kats <ilia-kats@gmx.net>
2016-06-10 12:22:31 +0200

    only cythonize in development mode

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 from setuptools import setup
 from setuptools.extension import Extension
 from setuptools.command.develop import develop
-from Cython.Build import cythonize
 
 readcounter = Extension("readcounter",
-        sources=["combinatorialprofiler/readcounter/readcounter.pyx", "combinatorialprofiler/readcounter/cReadCounter.cpp"],
+        sources=["combinatorialprofiler/readcounter/readcounter.cpp", "combinatorialprofiler/readcounter/cReadCounter.cpp"],
         language="c++",
         extra_compile_args=["-std=c++14"],
         extra_link_args=["-std=c++14"]
     )
 
-debug = False
-
 class debugmode(develop):
     def __init__(self, dist, **kw):
         super().__init__(dist, **kw)
-        debug=True
+
+        from Cython.Build import cythonize
+        import os.path
+        global readcounter
         readcounter.extra_compile_args.append("-O0")
+        path, ext = os.path.splitext(readcounter.sources[0])
+        readcounter.sources[0] = path + ".pyx"
+        readcounter = cythonize(readcounter)
 
 
 setup(name='CombinatorialProfiler',
@@ -29,7 +32,7 @@ setup(name='CombinatorialProfiler',
     package_data={
         '': ['*.ui']
     },
-    ext_modules = cythonize(readcounter, gdb_debug=debug),
+    ext_modules = [readcounter],
     cmdclass={
         'develop': debugmode
     }
