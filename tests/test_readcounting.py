930758b8f7841f536f01cb893364285649fdb8dc
Ilia Kats <ilia-kats@gmx.net>
2016-06-11 12:01:25 +0200

    finish writing tests for barcode/insert matching
    still need to test unmatchable reads

--- tests/test_readcounting.py
+++ tests/test_readcounting.py
@@ -18,25 +18,25 @@ class FastQCreator:
         return seq.translate(cls.ttable)[::-1]
 
     def __init__(self, tmpdir, ninserts=2):
-        self.totalreads = random.randrange(1000, 10001)
+        self.totalreads = random.randint(1000, 10000)
         self.fwcodes = set()
         self.revcodes = set()
         self.inserts = set()
-        self.insertlength = random.randrange(100, 301)
-        self.varlength = random.randrange(6, 10)
+        self.insertlength = random.randint(100, 300)
+        self.varlength = random.randint(6, 10)
 
         self.varcounts = [{} for i in range(ninserts)]
         self.unique_inserts = set()
 
         self.file = tmpdir.join("%i.fastq" % time.time())
 
-        ncodes = random.randrange(1, 10)
+        ncodes = random.randint(4, 10)
         while len(self.fwcodes) != ncodes:
-            self.fwcodes.add("".join([self.getnuc() for n in range(random.randrange(6,20))]))
+            self.fwcodes.add("".join([self.getnuc() for n in range(random.randint(6,20))]))
 
-        ncodes = random.randrange(1, 10)
+        ncodes = random.randint(4, 10)
         while len(self.revcodes) != ncodes:
-            self.revcodes.add("".join([self.getnuc() for n in range(random.randrange(6,20))]))
+            self.revcodes.add("".join([self.getnuc() for n in range(random.randint(6,20))]))
         self.fwcodes = list(self.fwcodes)
         self.revcodes = list(self.revcodes)
 
@@ -65,14 +65,17 @@ class FastQCreator:
                 print("+", file=fq)
                 print("I" * (len(fwcode) + len(insert) + len(revcode)), file=fq)
 
+def make_barcodes_dict(codes):
+    return {str(i): c for i,c in enumerate(codes)}
+
 def test_simplecounts(tmpdir):
     fq = FastQCreator(tmpdir)
     exps = []
     for i in enumerate(fq.inserts):
         d = {}
         d['insert'] = i[1]
-        d['barcodes_fw'] = {str(i): c for i,c in enumerate(fq.fwcodes)}
-        d['barcodes_rev'] = {str(i): c for i,c in enumerate(fq.revcodes)}
+        d['barcodes_fw'] = make_barcodes_dict(fq.fwcodes)
+        d['barcodes_rev'] = make_barcodes_dict(fq.revcodes)
         exps.append(PyExperiment(str(i[0]), d))
     counter = PyReadCounter(exps)
 
@@ -98,7 +101,7 @@ def test_namedinserts(tmpdir):
                 totalcounts += counts
             else:
                 unmatched += counts
-    d = {'insert': fq.inserts[0], 'barcodes_fw': dict((str(i), c) for i,c in enumerate(fq.fwcodes)), 'barcodes_rev': dict((str(i), c) for i,c in enumerate(fq.revcodes)), 'named_inserts': nins}
+    d = {'insert': fq.inserts[0], 'barcodes_fw': make_barcodes_dict(fq.fwcodes), 'barcodes_rev': make_barcodes_dict(fq.revcodes), 'named_inserts': nins}
     exp = PyExperiment('test', d)
     counter = PyReadCounter([exp])
 
@@ -114,3 +117,95 @@ def test_namedinserts(tmpdir):
     assert counter.unmatched_insert_sequence == unmatched
     assert counter.unmatched_total == counter.unmatched_insert_sequence
     assert exp.counts == ncounts
+
+def test_no_reverse_codes(tmpdir):
+    fq = FastQCreator(tmpdir, ninserts=2)
+    counts = fq.varcounts
+    counts.append({})
+    revcodes_tokeep = {str(i) : fq.revcodes[i] for i in random.sample(range(len(fq.revcodes)), int(0.7 * len(fq.revcodes)))}
+    for codes in list(counts[-2].keys()):
+        if codes[1] not in revcodes_tokeep:
+            nkey = (codes[0], '')
+            ndict = counts[-1].setdefault(nkey, {})
+            for i, c in counts[-2][codes].items():
+                ndict[i] = ndict.get(i, 0) + c
+            del counts[-2][codes]
+    fwcodes = make_barcodes_dict(fq.fwcodes)
+    exps = [
+        PyExperiment("fullset", {'insert': fq.inserts[0], 'barcodes_fw':  fwcodes, 'barcodes_rev': make_barcodes_dict(fq.revcodes)}),
+        PyExperiment("reduced", {'insert': fq.inserts[1], 'barcodes_fw': fwcodes, 'barcodes_rev': revcodes_tokeep}),
+        PyExperiment("norevcodes", {'insert': fq.inserts[1], 'barcodes_fw': fwcodes})
+    ]
+    counter = PyReadCounter(exps)
+
+    unmatched = tmpdir.mkdir("%s_unmatched" % fq.file.basename)
+    counter.countReads(str(fq.file), str(unmatched), 4)
+    assert counter.read == fq.totalreads
+    assert counter.counted == fq.totalreads
+    for val, truth in zip(exps, counts):
+        assert val.counts == truth, "Counts don't match with expected values for experiment %s" % val.name
+
+def test_no_forward_codes(tmpdir):
+    fq = FastQCreator(tmpdir, ninserts=2)
+    counts = fq.varcounts
+    counts.append({})
+    fwcodes_tokeep = {str(i) : fq.fwcodes[i] for i in random.sample(range(len(fq.fwcodes)), int(0.7 * len(fq.fwcodes)))}
+    for codes in list(counts[-2].keys()):
+        if codes[0] not in fwcodes_tokeep:
+            nkey = ('', codes[1])
+            ndict = counts[-1].setdefault(nkey, {})
+            for i, c in counts[-2][codes].items():
+                ndict[i] = ndict.get(i, 0) + c
+            del counts[-2][codes]
+    revcodes = make_barcodes_dict(fq.revcodes)
+    exps = [
+        PyExperiment("fullset", {'insert': fq.inserts[0], 'barcodes_rev':  revcodes, 'barcodes_fw': make_barcodes_dict(fq.fwcodes)}),
+        PyExperiment("reduced", {'insert': fq.inserts[1], 'barcodes_rev': revcodes, 'barcodes_fw': fwcodes_tokeep}),
+        PyExperiment("nofwcodes", {'insert': fq.inserts[1], 'barcodes_rev': revcodes})
+    ]
+    counter = PyReadCounter(exps)
+
+    unmatched = tmpdir.mkdir("%s_unmatched" % fq.file.basename)
+    counter.countReads(str(fq.file), str(unmatched), 4)
+    assert counter.read == fq.totalreads
+    assert counter.counted == fq.totalreads
+    for val, truth in zip(exps, counts):
+        assert val.counts == truth, "Counts don't match with expected values for experiment %s" % val.name
+
+def test_no_forward_and_reverse_codes(tmpdir):
+    fq = FastQCreator(tmpdir, ninserts=2)
+    counts = fq.varcounts
+    counts.extend([{}, {}, {}])
+    fwcodes_tokeep = {str(i) : fq.fwcodes[i] for i in random.sample(range(len(fq.fwcodes)), int(0.7 * len(fq.fwcodes)))}
+    revcodes_tokeep = {str(i) : fq.revcodes[i] for i in random.sample(range(len(fq.revcodes)), int(0.7 * len(fq.revcodes)))}
+
+    for codes in list(counts[-4].keys()):
+        ncodes = codes
+        index = -4
+        if ncodes[0] not in fwcodes_tokeep:
+            ncodes = ('', ncodes[1])
+            index += 1
+        if ncodes[1] not in revcodes_tokeep:
+            ncodes = (ncodes[0], '')
+            index += 2
+        if ncodes not in counts[-4]:
+            ndict = counts[index].setdefault(ncodes, {})
+            for i, c in counts[-4][codes].items():
+                ndict[i] = ndict.get(i, 0) + c
+            del counts[-4][codes]
+
+    exps = [
+        PyExperiment("fullset", {'insert': fq.inserts[0], 'barcodes_fw':  make_barcodes_dict(fq.fwcodes), 'barcodes_rev': make_barcodes_dict(fq.revcodes)}),
+        PyExperiment("reduced", {'insert': fq.inserts[1], 'barcodes_fw': fwcodes_tokeep, 'barcodes_rev': revcodes_tokeep}),
+        PyExperiment("nofwcodes", {'insert': fq.inserts[1], 'barcodes_rev': revcodes_tokeep}),
+        PyExperiment("norevcodes", {'insert': fq.inserts[1], 'barcodes_fw': fwcodes_tokeep}),
+        PyExperiment("nofwrevcodes", {'insert': fq.inserts[1]})
+    ]
+    counter = PyReadCounter(exps)
+
+    unmatched = tmpdir.mkdir("%s_unmatched" % fq.file.basename)
+    counter.countReads(str(fq.file), str(unmatched), 4)
+    assert counter.read == fq.totalreads
+    assert counter.counted == fq.totalreads
+    for val, truth in zip(exps, counts):
+        assert val.counts == truth, "Counts don't match with expected values for experiment %s" % val.name
