GUI: fix floating point precision problem
the solution is somewhat ugly, we first read in a dataframe with
dtype conversion to determine if it's in melted format or not,
and than read in a second dataframe but keeping dtype=str, and
using that one for manipulations

combinatorialprofiler/csvio.py
@@ -15,10 +15,10 @@ def get_csv_reader(f):
     dialect = s.sniff(sample)
     has_header = s.has_header(sample)
     reader = csv.reader(f, dialect)
-    return (reader, has_header)
+    return (reader, has_header, dialect)
 
 def readBarcodes(f):
-    reader, has_header = get_csv_reader(f)
+    reader, has_header, dialect = get_csv_reader(f)
     if has_header:
         next(reader)
     tr = str.maketrans('', '', 'ATCG')
@@ -40,13 +40,30 @@ def readBarcodes(f):
 
 _unmatchedlabelsexception = RuntimeError("Barcode labels don't match sorted cells labels")
 
-def readCellCounts(f, barcodes_fw=None, barcodes_rev=None):
-    header = get_csv_reader(f)[1]
+def readCellCounts(f, barcodes_fw=None, barcodes_rev=None, force_string=False):
+    header, dialect = get_csv_reader(f)[1:3]
     if header:
         header = 0
     else:
         header = None
-    df = pd.read_csv(f, header=header, sep=None, engine='python')
+    df = pd.read_csv(f,
+                     header=header,
+                     sep=dialect.delimiter,
+                     doublequote=dialect.doublequote,
+                     escapechar=dialect.escapechar,
+                     quotechar=dialect.quotechar,
+                     skipinitialspace=dialect.skipinitialspace)
+    df_toreturn = df
+    if force_string:
+        f.seek(0)
+        df_toreturn = pd.read_csv(f,
+                                  header=header,
+                                  sep=dialect.delimiter,
+                                  doublequote=dialect.doublequote,
+                                  escapechar=dialect.escapechar,
+                                  quotechar=dialect.quotechar,
+                                  skipinitialspace=dialect.skipinitialspace,
+                                  dtype=str)
     types = df.get_dtype_counts()
     if types['float64'] != df.columns.size - 1:
         valuecol = np.where(df.dtypes == 'float64')[0]
@@ -69,23 +86,23 @@ def readCellCounts(f, barcodes_fw=None, barcodes_rev=None):
             index = df.columns[list(reversed(indexcols))].values.tolist()
         else:
             raise _unmatchedlabelsexception
-        toreturn = df.set_index(index)
+        toreturn = df_toreturn.set_index(index)
         toreturn.index.rename(['barcode_fw', 'barcode_rev'], inplace=True)
         toreturn = toreturn.loc[:,df.columns[valuecol]]
     else:
         df = df.set_index(df.columns[0])
         if barcodes_fw is not None and len(barcodes_fw):
             if not set(df.index).isdisjoint(barcodes_fw):
-                toreturn = df.stack()
+                toreturn = df_toreturn.stack()
             elif not set(df.columns).isdisjoint(barcodes_fw):
-                toreturn = df.T.stack()
+                toreturn = df_toreturn.T.stack()
             else:
                 raise _unmatchedlabelsexception
         elif barcodes_rev is not None and len(barcodes_rev):
-            if not set(df.index).isdisjoint(barcodes_rev):
-                toreturn = df.T.stack()
+            if not set(df_toreturn.index).isdisjoint(barcodes_rev):
+                toreturn = df_toreturn.T.stack()
             elif not set(df.columns).isdisjoint(barcodes_rev):
-                toreturn = df.stack()
+                toreturn = df_toreturn.stack()
             else:
                 raise _unmatchedlabelsexception
         else: